647e9857aff1143b4cde06d37259dbf5471052b7
Kub94ekCZ <denos.123@seznam.cz>
2024-12-02 22:45:14 +0100

    Initial Commit

---
+++ src/main/java/me/kub94ek/Main.java
@@ -0,0 +1,50 @@
+package me.kub94ek;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
+public class Main extends ListenerAdapter {
+    private static JDA jda;
+    
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            throw new RuntimeException("Missing arguments");
+        }
+        
+        new Main().startBot(args[0]);
+        
+    }
+    
+    public void startBot(String botToken) {
+        JDABuilder builder = JDABuilder.createLight(
+                botToken,
+                GatewayIntent.GUILD_MESSAGES, GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MEMBERS
+        );
+        
+        builder.setActivity(Activity.customStatus("Throwing CrimsonBalls on the table"));
+        
+        try {
+            jda = builder.build().awaitReady();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        
+        jda.addEventListener(this);
+        
+        jda.getGuilds().forEach(Main::registerGuildCommands);
+    }
+    
+    private static void registerGuildCommands(Guild server) {
+        // TODO: Command registering
+    }
+    
+    
+    public static JDA getJda() {
+        return jda;
+    }
+    
+}
---
+++ src/main/java/me/kub94ek/data/database/Database.java
@@ -0,0 +1,4 @@
+package me.kub94ek.data.database;
+
+public class Database {
+}
